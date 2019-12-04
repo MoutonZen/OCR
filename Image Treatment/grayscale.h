8bcf3e60d34f46d621d624435b1cc1bb44ed964c
pierreblais <pierre.blais@epita.fr>
2019-12-04 19:00:41 +0100

    .

---
+++ Image Treatment/grayscale.h
@@ -0,0 +1,11 @@
+#ifndef GRAYSCALE_H
+#define GRAYSCALE_H
+
+#include <stdlib.h>
+#include <err.h>
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+
+void grayscale(SDL_Surface* image_surface);
+
+#endif
